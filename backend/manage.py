d54bf4c5e5ece7faf6dc897e52d3434bd7800fc9
dannaemartinez <dannae.martinez@enroutesystems.com>
2022-05-09 15:25:26 +0000

    restore django normal debug

--- backend/manage.py
+++ backend/manage.py
@@ -8,13 +8,14 @@ def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'library.settings')
 
-    from django.conf import settings
-    if settings.DEBUG:
-        if os.environ.get('RUN_MAIN') or os.environ.get('WERKZEUG_RUN_MAIN'):
-            import debugpy
-            debugpy.listen(("0.0.0.0", 3001))
-            debugpy.wait_for_client()
-            print('Attached!')
+    # # Debug configuration of debugpy (when enabled hangs up the normal behabiour of django)
+    # from django.conf import settings
+    # if settings.DEBUG:
+    #     if os.environ.get('RUN_MAIN') or os.environ.get('WERKZEUG_RUN_MAIN'):
+    #         import debugpy
+    #         debugpy.listen(("0.0.0.0", 3001))
+    #         debugpy.wait_for_client()
+    #         print('Attached!')
 
     try:
         from django.core.management import execute_from_command_line
